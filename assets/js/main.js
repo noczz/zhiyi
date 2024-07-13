fd701847d84fb60a9020f3a47280e385050f51f8
nocz <767714990@qq.com>
2024-07-13 14:26:33 +0800

    Add preloader

--- assets/js/main.js
+++ assets/js/main.js
@@ -47,6 +47,20 @@
   });
 
   /**
+   * Toggle mobile nav dropdowns
+   */
+  document.querySelectorAll('.navmenu .toggle-dropdown').forEach(navmenu => {
+    navmenu.addEventListener('click', function(e) {
+      if (document.querySelector('.mobile-nav-active')) {
+        e.preventDefault();
+        this.parentNode.classList.toggle('active');
+        this.parentNode.nextElementSibling.classList.toggle('dropdown-active');
+        e.stopImmediatePropagation();
+      }
+    });
+  });
+
+  /**
    * Preloader
    */
   const preloader = document.querySelector('#preloader');
